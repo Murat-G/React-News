b96b04485905774489f96558cebe423872171f53
MuratG <muratgozderesi34@gmail.com>
2021-01-22 23:33:42 +0300

    completed search page

--- client/src/pages/search/Search.js
+++ client/src/pages/search/Search.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import Input from './components/SearchInput'
+import SearchInput from './components/SearchInput'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import "./Search.css"
 
 function Search() {
     return (
-        <div className="containerSearch-input">
-            <div style={{display:"flex", flexDirection:"row", backgroundColor:"black", justifyContent:"center",width:"400px"}}>
-                <Input />
-                <div className="elementIcons-search"> <FontAwesomeIcon icon={faSearch} color="white" size="2x"/></div>
+        <div className="MainContainer-search">
+            <div className="containerSearch">
+                    <SearchInput />
+                    <div className="contentSearch-icon"> <FontAwesomeIcon icon={faSearch} color="white" size="1.5x"/> </div> 
             </div>
         </div>
     )
